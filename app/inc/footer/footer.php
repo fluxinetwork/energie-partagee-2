80305d52325e17e841487086e69c72d3211a30fe
Rolland Yann <rollandyann@gmail.com>
2016-02-01 14:20:28 +0100

    Home grid
    Build a flex grid

--- app/inc/footer/footer.php
+++ app/inc/footer/footer.php
@@ -10,3 +10,46 @@ if ( function_exists('is_mobile') ) {
 	is_mobile() ? $bodyclass .= ' is-mobile' : $bodyclass .= ' is-desktop';
 }
 ?>
+
+<footer class="footer">
+    <div class="wrap-n">
+    	<div class="box-flex">
+            <div class="box--half">
+                <h2>Nous suivre</h2>
+                <ul class="social">
+                    <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/ico-facebook.png"></a></li>
+                   <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/ico-twitter.png"></a></li>
+                   <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/ico-youtube.png"></a></li>
+                </ul>    
+            </div>
+            <form class="box--half" method="post" action="http://ymlp.com/subscribe.php?id=gbuyheegmgb" target="_blank">
+                <h2>Abonnement newsletter</h2> 
+                <input class="in-contact" name="YMP0" id="nom_contact" type="email" value="" placeholder="Votre email ici" required aria-required="true">
+                <button type="submit" class="button-round"><i>&radic;</i></button>
+            </form>            
+        </div>
+        
+        <h2>Nos partenaires</h2>
+        <ul class="partenaires box-flex">
+            <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logo-partenaire-1.jpg"></a></li>
+            <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logo-partenaire-2.jpg"></a></li>
+            <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logo-partenaire-3.jpg"></a></li>
+            <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logo-partenaire-4.jpg"></a></li>
+            <li><a class="button-round" href="#"><i>+</i></a></li> 
+        </ul>
+        
+        <h2>Nos labels</h2>
+        <ul class="labels box-flex">
+            <li class="labels--img"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logo-label-1.png"></a></li>
+            <li class="labels--img"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logo-label-2.png"></a></li>
+            <li class="labels--txt"><p>Energie Partagée a recu les labels Finansol et Agrément “entreprise solidaire” pour son activité engagée bla bla bla et bla bla bla.</p></li>
+        </ul>
+        
+        <nav class="nav-footer">
+        	<a href="#">Contact</a>
+           <a href="#">Mentions légales </a>
+           <a href="#">Crédits</a>
+        </nav>
+        
+    </div>    
+</footer>
--- home.php
+++ home.php
@@ -1,21 +1,139 @@
 <?php get_header(); ?>
 <section>
-	<article class="first-bloc">
-        <h1>Investissons les énergies renouvelables</h1>
-        <ul class="key-number">
-          <li><span class="number">5000</span> actionnaires citoyens</li>
-          <li><span class="number">25</span> projets locaux financés</li>
-          <li><span class="number">+10</span> millions d’euros collectés</li>      
-        </ul>
-        <button type="button" class="button white">Comprendre notre action</button>
+	<article class="header-bloc">
+    	<div class="wrap-l">
+            <h1>Investissons les énergies renouvelables</h1>
+            <ul class="key-numbers">
+              <li><span class="number">5000</span> actionnaires<br>citoyens</li>
+              <li><span class="number">25</span> projets locaux<br>financés</li>
+              <li><span class="number">+10</span> millions d’euros<br>collectés</li>      
+            </ul>
+            <a href="#" class="button">Comprendre notre action</a>
+        </div>
 	</article>
     
-    <article class="highlight-box">
-    	<p>Actualité importante</p>
-    	<h1>Levée de fonds historique</h1>
-        <p>Au milieu de la forêt de Raon-L’Etape (88) coule la Meurthe, qui jadis était utilisée pour produire de l’électricité. Démantelée il y a quinze ans, la petite centrale hydraulique reprend vie aujourd’hui après plusieurs mois de travaux et la mobilisation des souscripteurs Énergie Partagée. Retour en photo sur cette réhabilitation.</p>
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/ico-arrow-next.png"></a>
+    <article class="wrap-highlight">
+    	<div class="wrap-n">
+            <p class="lighter">Actualité importante</p>
+            <h1>Levée de fonds historique</h1>
+            <div class="box-flex">
+            	<div class="box--solo">
+                <p class="highlight-txt">Au milieu de la forêt de Raon-L’Etape (88) coule la Meurthe, qui jadis était utilisée pour produire de l’électricité. Démantelée il y a quinze ans, la petite centrale hydraulique reprend vie aujourd’hui après plusieurs mois de travaux et la mobilisation des souscripteurs Énergie Partagée. Retour en photo sur cette réhabilitation.</p>
+               </div> 
+                <div class="box--fixe"><a class="button-round" href="#"><i>&rarr;</i></a></div>
+            </div>            
+        </div>
     </article>
+    
+    <article class="wrap-l steps-intro">
+        <h2>Énergie partagée en 3 étapes</h2>
+        <ul class="box-flex">
+            <li class="flex-item">
+                <div class="holder-img"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/illu-step.png"></div>         		
+                <p>Des projets citoyens d'énergie renouvelable émergent partout en France.</p>        
+                <a href="#">Voir les projets</a>
+            </li>
+            <li class="flex-item">
+                <div class="holder-img"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/illu-step.png"></div>	
+                <p>Énergie Partagée les accompagne et vous propose d'investir dans ces projets.</p>
+                <a href="#">Devenir actionnaire</a>
+            </li>   
+            <li class="flex-item">
+                <div class="holder-img"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/illu-step.png"></div>
+                <p>Energie verte, lutte contre l’effet de serre et revenus pour les actionnaires à la clé.</p>
+                <a href="#">Comment ça marche ?</a>
+            </li>
+        </ul>
+    </article>
+    
+    <article class="last-projects">
+    	<h2>Sélection de projets</h2>
+        <div class="wrap-n">        
+            <div class="box-flex">
+              <div class="box"><a href="#"><img class="img-reponsive" src="<?php echo get_template_directory_uri(); ?>/app/img/proto/img-projet-big.jpg"></a></div>
+              <div class="box--half"><a href="#"><img class="img-reponsive" src="<?php echo get_template_directory_uri(); ?>/app/img/proto/img-projet.jpg"></a></div>
+              <div class="box--half"><a href="#"><img class="img-reponsive" src="<?php echo get_template_directory_uri(); ?>/app/img/proto/img-projet.jpg"></a></div>
+              <div class="box--fixe">
+                    <a class="button-round grey" href="#"><i>></i></a> 
+              </div>          
+            </div>
+            <a href="#" class="button green">Voir la carte des projets</a>
+        </div>       
+        
+    </article>
+    
+    <article class="last-news">
+    	<div class="wrap-n">
+        	<div class="box-flex head-flex">
+            	<div class="box--half">
+            		<h2>Nos actualités</h2>
+            	</div>
+            	<div class="box--half">
+                    Suivez-nous sur 
+                    <ul class="social">
+                        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/ico-facebook.png"></a></li>
+                        <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/app/img/proto/ico-twitter.png"></a></li>
+                    </ul>
+           	</div>
+             	<div class="box--fixe"></div>
+        	</div>
+       
+           <div class="box-flex">         
+                <div class="box--half">
+                   <a class="news-item" href="#">
+                       <img class="img-reponsive" src="<?php echo get_template_directory_uri(); ?>/app/img/proto/img-news.jpg">
+                       <span class="news-date">12 jan</span>
+                       <p>5 raisons d’offrir des actions Énergie Partagée à Noël</p>
+                   </a>
+                </div>
+                <div class="box--half">
+                    <a class="news-item" href="#">
+                       <img class="img-reponsive" src="<?php echo get_template_directory_uri(); ?>/app/img/proto/img-news.jpg">
+                       <span class="news-date">23 dec</span>
+                       <p>5 raisons d’offrir des actions Énergie Partagée à Noël</p>
+                   </a>
+                </div>
+                <div class="box--fixe"> 
+                    <a class="button-round grey" href="#"><i>+</i></a> 
+                </div>
+           </div>
+       </div>
+    </article>
+    
+    <aside class="courtcircuit">
+    	<h3><span class="deco-left"><img class="img-svg" src="<?php echo get_template_directory_uri(); ?>/app/img/deco-left.svg"></span>Court&nbsp;Circuit<span class="deco-right"><img class="img-svg" src="<?php echo get_template_directory_uri(); ?>/app/img/deco-right.svg"></span>
+        </h3>
+        
+        <form class="wrap-n" method="post" action="http://ymlp.com/subscribe.php?id=gbuyheegmgb" target="_blank"> 
+        <div class="box-flex">
+          <div class="box--half">
+          		<p>Abonnez-vous a Court-Circuit, la newsletter d’Energie Partagée.</p>
+          </div>
+          <div class="box--half">
+          		<input class="courtcircuit--input" name="YMP0" id="courtcircuit_contact" type="email" value="" placeholder="Votre email ici" required aria-required="true">
+          </div>
+          <div class="box--fixe">
+            	<button type="submit" class="button-round"><i>&radic;</i></button>
+          </div>          
+        </div>
+        </form>
+    </aside>
+    
+    <article class="top-credibility">
+    	<div class="wrap-n">
+            <h2>Ils parlent de nous</h2>
+            <div class="box-flex">
+            	 <div class="box--solo">
+                    <img class="img-reponsive" src="<?php echo get_template_directory_uri(); ?>/app/img/proto/logos-presse.jpg">
+                </div>
+                <div class="box--fixe">
+                    <a class="button-round grey" href="#"><i>+</i></a>
+                </div>
+            </div>
+        </div> 
+    </article>
+        
+    
     
 </section>
 
