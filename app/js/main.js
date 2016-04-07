b458ef995abd6d12621d534c69f40ee539f47be3
Rolland Yann <rollandyann@gmail.com>
2016-04-07 19:45:23 +0200

    Spinner form prospect
    Spinner on submit form prospect

--- app/js/main.js
+++ app/js/main.js
@@ -1661,14 +1661,16 @@ function initSendMailPorspect (){
 	$('body').on('submit', 'form#mailing_prospect', function(e){  
 			  
 		e.preventDefault();	
-		
+
+		$('#mailing_prospect .button-round i').attr('class','spinner');
+
 		 $.ajax({
 			type: 'POST',
 			dataType: 'JSON',
 			url: ajax_object.ajax_url,
 			data: $(this).serialize()+'&action=send_mail_prospect',
 			success: function(data){
-				
+				$('#mailing_prospect .button-round i').attr('class','icon-check_64');
 				$('#mailing_prospect button').attr('disabled',true);							
 				if(data[0].validation == 'error'){					
 					$('#mailing_prospect button').attr('disabled',false);
