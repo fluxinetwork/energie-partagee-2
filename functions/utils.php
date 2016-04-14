e1a42668b703f9d1d339158a01f41b4b33467890
Rolland Yann <rollandyann@gmail.com>
2016-04-14 17:18:53 +0200

    Category load more
    Img size fix

--- functions/utils.php
+++ functions/utils.php
@@ -256,7 +256,7 @@ function more_post_ajax(){
 		// Thumb
 		if ( has_post_thumbnail() ):
 			$news_img_id = get_post_thumbnail_id();
-			$news_img_array = wp_get_attachment_image_src($news_img_id, 'medium', true);
+			$news_img_array = wp_get_attachment_image_src($news_img_id, 'card--mini', true);
 			$news_img_url = $news_img_array[0];
            $news_img = '<img class="img-reponsive" src="'.$news_img_url.'">';
 		endif;
