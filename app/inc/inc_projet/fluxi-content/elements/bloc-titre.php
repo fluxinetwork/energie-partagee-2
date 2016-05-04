f5cf64f2ae7d76e7cea4de98c621e12de75e1d70
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2016-05-04 11:28:59 +0200

    Titres

--- app/inc/inc_projet/fluxi-content/elements/bloc-titre.php
+++ app/inc/inc_projet/fluxi-content/elements/bloc-titre.php
@@ -8,6 +8,10 @@
 		$class_titre = 3;
 	elseif($taille_titre == 3):
 		$class_titre = 4;
+	elseif($taille_titre == 4):
+		$class_titre = 4;
+	elseif($taille_titre == 5):
+		$class_titre = 4;
 	endif;
 	
 	echo '<h'.$taille_titre.' class="h'.$class_titre.'">'.$texte_titre.'</h'.$taille_titre.'>';
